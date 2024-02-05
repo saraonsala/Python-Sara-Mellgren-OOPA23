b4d7b7772579f823aa56d341fb9bfdbc3558db19
Sara Mellgren <sarasbetongochtradgard@gmail.com>
2024-02-05 14:03:22 +0100

    add graph

--- Test.py
+++ Test.py
@@ -1,1 +1,9 @@
-print("hej")
+import matplotlib.pyplot as plt
+numbers = list(range(10))
+squers = [number**2 for number in numbers]
+print(numbers)
+print (squers)
+
+plt.plot(numbers, squers)
+plt.show()
+
